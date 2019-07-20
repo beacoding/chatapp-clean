512e9ef033c2ddf1911932f8f6ee89c84347fbc6
bea <bea@polyglotcoding.com>
2019-07-20 10:51:13 -0700

    [refactor] Refactor code for simple chat app

--- server.py
+++ server.py
@@ -1,19 +1,14 @@
-from flask import render_template, Flask,  request, make_response,redirect
-
 import json
+from flask import render_template, Flask, request, make_response, redirect
 
 
-app = Flask(__name__,static_url_path='')
+APP = Flask(__name__, static_url_path='')
 
-@app.route('/chat')
-def hello_world(): 
+@APP.route('/chat')
+def chat():
     return render_template('index.html')
 
-@app.route('/cows')
-def cows():
-    return render_template('cows.html')
-
-@app.route("/post_new_message", methods = ["POST"])
+@APP.route("/post_new_message", methods=["POST"])
 def post_new_message():
     username = request.cookies.get('username')
     colour = request.cookies.get('colour')
@@ -24,34 +19,32 @@ def post_new_message():
     form_inputs = dict(request.form)
     form_inputs["user"] = username
     form_inputs["colour"] = colour
-    with open('messages.json', 'r+') as fp:
-        messages = json.load(fp)
+    with open('messages.json', 'r+') as f:
+        messages = json.load(f)
         messages.append(form_inputs)
-        fp.seek(0)
-        json.dump(messages, fp)
-        fp.truncate()
+        f.seek(0)
+        json.dump(messages, f)
+        f.truncate()
     return json.dumps(messages)
 
-@app.route("/get_all_messages")
+@APP.route("/get_all_messages")
 def get_message():
-    with open('messages.json', 'r') as fp:
-        messages = json.load(fp)
-        print(messages, "test")
+    with open('messages.json', 'r') as(f):
+        messages = json.load(f)
     return json.dumps(messages)
 
-
-
-@app.route('/',methods=["GET",  "POST"])
+@APP.route('/', methods=["GET", "POST"])
 def login():
     if request.method == 'POST':
-        username =  request.form["username"]
-        colour  =  request.form["colour"]
+        username = request.form["username"]
+        colour = request.form["colour"]
+
         resp = make_response(redirect('/chat', code=302))
         resp.set_cookie('username', username)
-        resp.set_cookie('colour',colour)
+        resp.set_cookie('colour', colour)
         return resp
     return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run()
+    APP.run()
 
--- static/chat.js
+++ static/chat.js
@@ -1,98 +1,97 @@
-$( document ).ready(function() {
-    var colours = {
-        "cb-1": "black",
-        "cb-2": "#7b7d7b",
-        "cb-3": "#7b0000",
-        "cb-4": "#7b7d01",
-        "cb-5": "#027d00",
-        "cb-6": "#027d7b",
-        "cb-7": "#05007b",
-        "cb-8": "#7b007b",
-        "cb-9": "#7b7d39",
-        "cb-10": "#003c39",
-        "cb-11": "#007dfe",
-        "cb-12": "#003c7b",
-        "cb-13": "#3900ff",
-        "cb-14": "#7b3c00",
-        "cb-15": "#ffffff",
-        "cb-16": "#bdbebd",
-        "cb-17": "#ff0000",
-        "cb-18": "#feff00",
-        "cb-19": "#01ff00",
-        "cb-20": "#01ffff",
-        "cb-21": "#1600ff",
-        "cb-22": "#ff00ff",
-        "cb-23": "#feff7b",
-        "cb-24": "#02ff7b",
-        "cb-25": "#7cfffe",
-        "cb-26": "#7b7dff",
-        "cb-27": "#ff007b",
-        "cb-28": "#ff7d38"
+$(document).ready(function() {
+  var scrolledDown = false;
+  var colour;
+  var colours = {
+    "cb-1": "black",
+    "cb-2": "#7b7d7b",
+    "cb-3": "#7b0000",
+    "cb-4": "#7b7d01",
+    "cb-5": "#027d00",
+    "cb-6": "#027d7b",
+    "cb-7": "#05007b",
+    "cb-8": "#7b007b",
+    "cb-9": "#7b7d39",
+    "cb-10": "#003c39",
+    "cb-11": "#007dfe",
+    "cb-12": "#003c7b",
+    "cb-13": "#3900ff",
+    "cb-14": "#7b3c00",
+    "cb-15": "#ffffff",
+    "cb-16": "#bdbebd",
+    "cb-17": "#ff0000",
+    "cb-18": "#feff00",
+    "cb-19": "#01ff00",
+    "cb-20": "#01ffff",
+    "cb-21": "#1600ff",
+    "cb-22": "#ff00ff",
+    "cb-23": "#feff7b",
+    "cb-24": "#02ff7b",
+    "cb-25": "#7cfffe",
+    "cb-26": "#7b7dff",
+    "cb-27": "#ff007b",
+    "cb-28": "#ff7d38"
+  }
 
-    }
 
-    for (var id in colours)  {
-        var colour_box = document.getElementById(id)
-        colour_box.style.backgroundColor = colours[id];
-    }
+  function displayMessages(x) {
+    var style = `${"'" + "color:" + x.colour + "'"}`;
+    $("#messages").append(`
+      <div class = "message">
+        <div class="username" style=${style}> ${x.user}</div>
+        <div class="text">${x.msg}</div>
+      </div>
+    `)
+  }
 
-    var colour;
 
-    $('.cb').click(function() {
-        console.log("clicked on cb");
-        colour =  colours[this.id]
+  function fetchMessages() {
+    $.get( "/get_all_messages", function(data) {
+      $("#messages").html("")
+
+      data = JSON.parse(data);
+      data.forEach((x) =>  {
+        displayMessages(x);
+      });
+
+      var cows = document.getElementById("messages");
+      if (scrolledDown === false) {
+        cows.scrollTop = cows.scrollHeight;
+        scrolledDown = true;
+      }
     });
+  }
 
+  for (var id in colours)  {
+    var colour_box = document.getElementById(id)
+    colour_box.style.backgroundColor = colours[id];
+  }
 
-     var scrolledDown = false;
-    function fetchMessages() {
-        $.get( "/get_all_messages", function(data) {
-            data = JSON.parse(data);
-            $("#messages").html("")
-            data.forEach((x) =>  {
-                var style = `${"'" + "color:" + x.colour + "'"}`;
-                $("#messages").append(`<div class = "message"><div class = "username" style = ${style}> ${x.user}</div> <div class = "text">${x.msg}</div></div>`)
-            });
-            var cows = document.getElementById("messages");
-            if (scrolledDown === false) {
-                cows.scrollTop = cows.scrollHeight;
-                scrolledDown = true;
-            }
-        });
-    }
-    setInterval(function(){ fetchMessages() }, 1000);
+  $('.cb').click(function() {
+    colour =  colours[this.id]
+  });
 
+  $("#formId").click(function(){
+    var msg = $("#chatbox").val();
 
-    $("#target").click(function() {
-        $.get( "/random_string", function(data) {
-            $( "#random-string" ).html( data );
-        });
-    });
+    // Post a new message to the route /post_new_message and also 
+    // give it the message and the user who sent  it !!
+    $.post("/post_new_message", {msg: msg,colour:colour}, function(data) {
+      $("#messages").html("")
+      $("#chatbox").val("")
 
-    $("#formId").click(function(){
-        var msg=$("#chatbox").val();
+        // Server gives us back a JSON string. We have to parse it to
+        // tturn it into a list
+        data = JSON.parse(data);
+        // For loop over every single messagee to display user and message
+        data.forEach((x) =>  {
+            // we add a new div to the html elemenet with id messages
+            displayMessages(x)
+          });
+        var cows = document.getElementById("messages");
 
-        // Post a new message to the route /post_new_message and also 
-        // give it the message and the user who sent  it !!
-        $.post("/post_new_message", {msg: msg,colour:colour}, function(data) {
-            $("#messages").html("")
-            $("#chatbox").val("")
-            
-            // Server gives us back a JSON string. We have to parse it to
-            // tturn it into a list
-            data = JSON.parse(data);
-            // For loop over every single messagee to display user and message
-            data.forEach((x) =>  {
-                console.log("This  is x:", x);
-                var style = `${"'" + "color:" + x.colour + "'"}`;
-                // we add a new div to the html elemenet with id messages
-                $("#messages").append(`<div class = "message"><div class = "username" style = ${style}> ${x.user}</div> <div class = "text" >${x.msg}</div></div>`)
-            });
-            var cows = document.getElementById("messages");
+        cows.scrollTop = cows.scrollHeight;
+    }) 
+  });
 
-            cows.scrollTop = cows.scrollHeight;
-        }) 
-    });
+  setInterval(function(){ fetchMessages() }, 1000);
 });
-
- 
